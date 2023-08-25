ad5f5cf46349b72dddbe74d5f77c4b2b8cc09ba0
MarcosHogs97 <115594562+MarcosHogs97@users.noreply.github.com>
2023-08-25 01:42:53 -0300

    Alistamento V1.5

--- AlistamentoMilitar/src/Aplication/StartAlistamentoMilitar.java
+++ AlistamentoMilitar/src/Aplication/StartAlistamentoMilitar.java
@@ -1,12 +1,20 @@
 package Aplication;
 
+import javax.swing.JOptionPane;
+
 import Entities.AlistamentoMilitar;
 
 public class StartAlistamentoMilitar {
 
 	public static void main(String[] args) {
-		AlistamentoMilitar alistamento = new AlistamentoMilitar(null,null,null,null,null,null,null,null,null);
-		alistamento.startAplication();
+		AlistamentoMilitar[] recrutas = new AlistamentoMilitar[2];
+
+		for (int i = 0; i < recrutas.length; i++) {
+			recrutas[i] = new AlistamentoMilitar("", "", "", "", "", "", "", "");
+			recrutas[i].cadastrarRecruta();
+		}
+		JOptionPane.showMessageDialog(null, "Informções do recruta 01\n" + recrutas[0].toString()
+				+ "\nInformções do recruta 02\n" + recrutas[1].toString());
 
 	}
 
--- AlistamentoMilitar/src/Entities/AlistamentoMilitar.java
+++ AlistamentoMilitar/src/Entities/AlistamentoMilitar.java
@@ -5,13 +5,8 @@ import javax.swing.JOptionPane;
 public class AlistamentoMilitar {
 	private String dadosPessoa1[] = new String[9];
 
-	/**
-	 * dados [0] = nome, dados [1] = Ano de nascimento, dados [2] = Nome da mãe, dados
-	 * [3] = Nome do pai, dados [4] = Telefone, dados [5] = Email ,dados [6] = Altura
-	 * ,dados [7] = Peso ,dados [8] = multa
-	 */
-	
-	public AlistamentoMilitar(String nome,String anoNacimento,String nomeMae,String nomePai,String telefone,String email, String altura, String peso,String multa) {
+	public AlistamentoMilitar(String nome, String anoNacimento, String nomeMae, String nomePai, String telefone,
+			String email, String altura, String peso) {
 		this.dadosPessoa1[0] = nome;
 		this.dadosPessoa1[1] = anoNacimento;
 		this.dadosPessoa1[2] = nomeMae;
@@ -20,114 +15,102 @@ public class AlistamentoMilitar {
 		this.dadosPessoa1[5] = email;
 		this.dadosPessoa1[6] = altura;
 		this.dadosPessoa1[7] = peso;
-		this.dadosPessoa1[8] = multa;
 	}
-	
-	// Gets
-	public String[] getDados() {
+
+	public String[] getDadosPessoa1() {
 		return dadosPessoa1;
 	}
 
-	public void startAplication() {
-		String[] options = { "Fazer Alistamento", "Alterar Telefone", "Visualizar Cadastro", "Sair" };
-		int choice = JOptionPane.showOptionDialog(null, "Selecione uma opção", "Menu", JOptionPane.DEFAULT_OPTION,
-				JOptionPane.DEFAULT_OPTION, null, options, null);
-		switch (choice) {
-		case 0:
-			informarAnoNacimento();
-			
-			break;
-		case 1:
-
-			break;
-		case 2:
-
-			break;
-		case 3:
-
-			break;
-		default:
-			break;
-		}
-
-	}
-	
-
-	
-
-	public void informarAnoNacimento() {
-		int anoNacimento = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o ano de nacimento"));
-		int idade = anoNacimento - 2023;
+	public void cadastrarRecruta() {
+		String anoNacimento = JOptionPane.showInputDialog("Informe o ano de nacimento");
+		int ano = Integer.parseInt(anoNacimento);
+		int idade = 2023 - ano;
 		if (idade >= 18 && idade < 45) {
-			multaPorIdade(idade);
 			anoNacimento(anoNacimento);
-			pesoEaltura();
-			informarNomes();
-		} else if (idade >= 45) {
-			JOptionPane.showMessageDialog(null, "A idade maxima para se alistar é de 45 anos");
+			String peso = JOptionPane.showInputDialog("Informe o peso");
+			peso(peso);
+			String altura = JOptionPane.showInputDialog("Informe o altura");
+			altura(altura);
+			String nome = JOptionPane.showInputDialog("Informe o nome");
+			nome(nome);
+			String nomeMae = JOptionPane.showInputDialog("Informe o nome da mae");
+			nomeMae(nomeMae);
+			String nomePai = JOptionPane.showInputDialog("Informe o nome do pai");
+			nomePai(nomePai);
+			String email = JOptionPane.showInputDialog("Informe o email para contato");
+			email(email);
+			String telefone = JOptionPane.showInputDialog("Informe o telefone para contato");
+			telefone(telefone);
+			double multa = calcularMulta();
+			if (multa > 0) {
+				String stringMulta = String.format("%.2f", multa);
+				multa(stringMulta);
+			}
+
+		} else if (idade > 45) {
+			JOptionPane.showMessageDialog(null, "Idade maxima para o alistamento é de 45 anos!!");
+		} else {
+			JOptionPane.showMessageDialog(null, "Idade minima para o alistamento é de 18 anos!!");
 		}
-		JOptionPane.showMessageDialog(null, "A idade minima para se alistar é de 18 anos");
+
 	}
 
-	public void multaPorIdade(int idade) {
-		if (idade > 20) {
-			double multa = (idade - 18) * 5.00;
-		}
+	public double calcularMulta() {
+		int idade = 2023 - Integer.parseInt(getDadosPessoa1()[1]);
+		double multa = (idade - 19) * 5;
+		return multa;
 	}
 
-	public void anoNacimento(int anoNacimento) {
-		dadosPessoa1[8] = String.format(null, anoNacimento);
-	}
-
-	public void pesoEaltura() {
-		String peso = JOptionPane.showInputDialog(null, "Informe o peso");
-		peso(peso);
-		String altura = JOptionPane.showInputDialog(null, "Informe a altura");
-		altura(altura);
-	}
-
-	public void peso(String peso) {
-		dadosPessoa1[8] = peso;
-	}
-
-	public void altura(String altura) {
-		dadosPessoa1[8] = altura;
-	}
-
-	public void informarNomes() {
-		String nome = JOptionPane.showInputDialog(null, "Informe o nome");
-		nome(nome);
-		String nomeMae = JOptionPane.showInputDialog(null, "Informe o nome da mae");
-		nomeMae(nomeMae);
-		String nomePai = JOptionPane.showInputDialog(null, "Informe o nome do pai");
-		nomePai(nomePai);
+	public void multa(String multa) {
+		this.dadosPessoa1[8] = multa;
 	}
 
 	public void nome(String nome) {
-		dadosPessoa1[1] = nome;
+		this.dadosPessoa1[0] = nome;
 	}
 
-	public void nomeMae(String nome) {
-		dadosPessoa1[3] = nome;
+	public void anoNacimento(String anoNacimento) {
+		this.dadosPessoa1[1] = anoNacimento;
 	}
 
-	public void nomePai(String nome) {
-		dadosPessoa1[4] = nome;
+	public void nomeMae(String nomeMae) {
+		this.dadosPessoa1[2] = nomeMae;
 	}
 
-	public void dadosDeContato() {
-		String telefone = JOptionPane.showInputDialog(null, "Informe o numero de telefone");
-		telefone(telefone);
-		String email = JOptionPane.showInputDialog(null, "Informe E-mai");
-		email(email);
+	public void nomePai(String nomePai) {
+		this.dadosPessoa1[3] = nomePai;
 	}
 
 	public void telefone(String telefone) {
-		dadosPessoa1[5] = telefone;
+		this.dadosPessoa1[4] = telefone;
 	}
 
 	public void email(String email) {
-		dadosPessoa1[6] = email;
+		this.dadosPessoa1[5] = email;
 	}
 
+	public void altura(String altura) {
+		this.dadosPessoa1[6] = altura;
+	}
+
+	public void peso(String peso) {
+		this.dadosPessoa1[7] = peso;
+	}
+
+	public String toString() {
+		double multa = calcularMulta();
+		if (multa > 0) {
+			return "Nome: " + getDadosPessoa1()[0] + "\n" + "Ano de nascimento: " + getDadosPessoa1()[1] + "\n"
+					+ "Nome da mãe: " + getDadosPessoa1()[2] + "\n" + "Nome do pai: " + getDadosPessoa1()[3] + "\n"
+					+ "Telefone: " + getDadosPessoa1()[4] + "\n" + "E-mail: " + getDadosPessoa1()[5] + "\n" + "Altura: "
+					+ getDadosPessoa1()[6] + "\n" + "Peso: " + getDadosPessoa1()[7] + "\n" + "Multa: "
+					+ getDadosPessoa1()[8] + "\n";
+		} else {
+			return "Nome: " + getDadosPessoa1()[0] + "\n" + "Ano de nascimento: " + getDadosPessoa1()[1] + "\n"
+					+ "Nome da mãe: " + getDadosPessoa1()[2] + "\n" + "Nome do pai: " + getDadosPessoa1()[3] + "\n"
+					+ "Telefone: " + getDadosPessoa1()[4] + "\n" + "E-mail: " + getDadosPessoa1()[5] + "\n" + "Altura: "
+					+ getDadosPessoa1()[6] + "\n" + "Peso: " + getDadosPessoa1()[7];
+		}
+
+	}
 }
